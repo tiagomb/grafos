c6d006ae81b9ce548cb9f3fea546a1bc0963e8e4
tiagomb <tmb22@inf.ufpr.br>
2024-06-13 17:18:37 -0300

    backtrack working, graph not yet

--- trabalho-1/rainhas.c
+++ trabalho-1/rainhas.c
@@ -4,31 +4,6 @@
 #include <time.h>
 #include <string.h>
 
-unsigned int *remove_vizinhos(unsigned int *conjunto, casa c, unsigned int n){
-    unsigned int *diferenca = (unsigned int *) malloc(n*n * sizeof(unsigned int));
-    memcpy(diferenca, conjunto, n*n*sizeof(unsigned int));
-    for (unsigned int i = 0; i < n*n; i++){
-        if (i % n == c.coluna - 1 || i / n == c.linha || i % n + i / n == c.coluna + c.linha - 1 || i % n - i / n == c.coluna - c.linha - 1){
-            diferenca[i] = 1;
-        }
-    }
-    return diferenca;
-}
-
-//--------------------------------------------------------------  ----------------
-// computa uma resposta para a instância (n,c) do problema das n rainhas 
-// com casas proibidas usando backtracking
-//
-//    n é o tamanho (número de linhas/colunas) do tabuleiro
-//
-//    c é um vetor de k 'struct casa' indicando as casas proibidas
-//
-//    r é um vetor de n posições (já alocado) a ser preenchido com a resposta:
-//      r[i] = j > 0 indica que a rainha da linha i+1 fica na coluna j;
-//      r[i] = 0     indica que não há rainha nenhuma na linha i+1
-//
-// devolve r
-
 int ehPossivel (unsigned int *r, unsigned int linha, unsigned int coluna, casa *c, unsigned int k) {
   for (unsigned int i = 0; i < linha; i++) {
     if (r[i] == coluna) {
@@ -48,167 +23,90 @@ int ehPossivel (unsigned int *r, unsigned int linha, unsigned int coluna, casa *
   return 1;
 }
 
-casa acha_linha_conjunto (unsigned int *conjunto, unsigned int n){
-  for (unsigned int i = 0; i < n; i++){
-    for (unsigned int j = 0; j < n; j++){
-      if (conjunto[i*n +j] == 0){
-        conjunto[i*n + j] = 1;
-        return (casa) {i, j + 1};
-      }
-    }
-  }
-  return (casa) {n, n + 1};
-
-}
-
-unsigned int retorna_tamanho_conjunto(unsigned int *conjunto, unsigned int n){
-  unsigned int tamanho = 0;
-  for (unsigned int i = 0; i < n; i++){
-      if (conjunto[i] == 0){
-        tamanho++;
-      }
-  }
-  return tamanho;
-}
-
-unsigned int retorna_tamanho(unsigned int *r, unsigned int n){
-  unsigned int tamanho = 0;
-  for (unsigned int i = 0; i < n; i++){
-      if (r[i]){
-        tamanho++;
-      }
-  }
-  return tamanho;
-}
-
-unsigned int retorna_linha(unsigned int *r, unsigned int n, unsigned int *tabuleiro) {
-  for (unsigned int i = 0; i < n; i++) {
-    if (r[i] == 0) {
-      for (unsigned int j = 0; j < n; j++) {
-        if (tabuleiro[i * n + j] == 0) 
-          return i;
-      }
-    }
-  }
-  return n;
-}
-
-unsigned int rainhas_bt_wrapped(unsigned int n, unsigned int k, casa *c, unsigned int *r, unsigned int *tabuleiro) {
-  unsigned int linha, aux;
-  linha = retorna_linha(r, n, tabuleiro);
-  if (linha == n) {
+unsigned int rainhas_bt_wrapped(unsigned int n, unsigned int k, casa *c, unsigned int *r, unsigned int linha, unsigned int cont){
+  if (linha == n){
     return 1;
   }
-  for (unsigned int i = 1; i <= n; i++) {
-    if (ehPossivel(r, linha, i, c, k)) {
-      r[linha] = i;
-      aux = rainhas_bt_wrapped(n, k, c, r, tabuleiro);
-      if (aux)
-        return aux;
-      r[linha] = 0;
+  while (cont){
+    for (unsigned int i = 1; i <= n; i++){
+      if (ehPossivel(r, linha, i, c, k)){
+        r[linha] = i;
+        if (rainhas_bt_wrapped(n, k, c, r, linha + 1, cont)){
+          return 1;
+        }
+        r[linha] = 0;
+      }
+    }
+    cont--;
+    linha++;
+    if (linha == n){
+      return 0;
     }
   }
   return 0;
 }
 
-unsigned int *rainhas_bt(unsigned int n, unsigned int k, casa *c, unsigned int *r) {
-  unsigned int *tabuleiro = (unsigned int *) calloc(n*n, sizeof(unsigned int));
-  unsigned int contadorcol = 0, contadorlin = 0;
-  for (unsigned int i = 0; i < k; i++){
-    tabuleiro[(c[i].linha - 1) * n + c[i].coluna - 1] = 1;
-  }
-  for (unsigned int i = 0; i < n; i++) {
-    unsigned int contcol = 0, contlin = 0;
+unsigned int *rainhas_bt(unsigned int n, unsigned int k, casa *c, unsigned int *r){
+  for (unsigned int i = 0; i < n; i++){
     r[i] = 0;
-    for (unsigned int j = 0; j < n; j++) {
-      if (tabuleiro[j * n + i]) {
-        contcol++;
-      }
-      if (tabuleiro[i * n + j]) {
-        contlin++;
-      }
-    }
-    if (contcol == n){
-      contadorcol++;
-    }
-    if (contlin == n){
-      contadorlin++;
+  }
+  for (unsigned int i = 0; i < n; i++){
+    if (rainhas_bt_wrapped(n, k, c, r, 0, i)){
+      return r;
     }
   }
-  if (contadorcol > contadorlin){
-    for (unsigned int i = 1; i <= contadorcol - contadorlin; i++){
-      for (unsigned int j = 0; j < n; j++){
-        tabuleiro[i*n + j] = 1;
-      }
-    }
-  }
-  rainhas_bt_wrapped(n, k, c, r, tabuleiro);
-  free(tabuleiro);
-  return r; 
+  return r;
 }
-//------------------------------------------------------------------------------
-// computa uma resposta para a instância (n,c) do problema das n
-// rainhas com casas proibidas usando a modelagem do problema como
-// conjunto independente de um grafo
-//
-// n, c e r são como em rainhas_bt()
 
-unsigned int rainhas_ci_wrapped(unsigned int *conjunto, unsigned int n, unsigned int *r, unsigned int *tabuleiro) {
-  unsigned int aux;
-  unsigned int *aux_conjunto;
-  unsigned int tamanho  = retorna_linha(r, n, tabuleiro);
-  unsigned int tamanho_conjunto = retorna_tamanho_conjunto(conjunto, n * n);
-  if (tamanho == n){
+void marca_vizinhos(unsigned int *tabuleiro, unsigned int n, unsigned int linha, unsigned int coluna, unsigned int marcador){
+    tabuleiro[linha * n + coluna - 1] = 1;
+    for (unsigned int i = 0; i < n*n; i++){
+        if (i % n == coluna - 1 || i / n == linha || i % n + i / n == coluna + linha - 1 || i % n - i / n == coluna - linha - 1){
+            tabuleiro[i] = marcador;
+        }
+    }
+}
+
+unsigned int rainhas_ci_wrapped(unsigned int n, unsigned int k, casa *c, unsigned int *r, unsigned int *tabuleiro, unsigned int linha, unsigned int cont){
+  if (linha == n){
     return 1;
   }
-  if (tamanho_conjunto + tamanho < n){
-    return 0;
+  while (cont){
+    for (unsigned int i = 1; i <= n; i++){
+      if (tabuleiro[(linha) * n + i - 1] == 0){
+        r[linha] = i;
+        marca_vizinhos(tabuleiro, n, linha, i, 1);
+        if (rainhas_ci_wrapped(n, k, c, r, tabuleiro, linha + 1, cont)){
+          return 1;
+        }
+        marca_vizinhos(tabuleiro, n, linha, i, 0);
+        r[linha] = 0;
+      }
+    }
+    cont--;
+    linha++;
+    if (linha == n){
+      return 0;
+    }
   }
-  casa c = acha_linha_conjunto(conjunto, n);
-  r[c.linha] = c.coluna;
-  aux_conjunto = remove_vizinhos(conjunto, c, n);
-  aux = rainhas_ci_wrapped(aux_conjunto, n, r, tabuleiro);
-  free(aux_conjunto);
-  if (aux) {
-    return aux;
-  }
-  r[c.linha] = 0;
-  return rainhas_ci_wrapped(conjunto, n, r, tabuleiro);
+  return 0;
+
 }
 
-
-unsigned int *rainhas_ci(unsigned int n, unsigned int k, casa *c, unsigned int *r) {
-  unsigned int contadorcol = 0, contadorlin = 0;
-  unsigned int *conjunto = (unsigned int *) calloc(n * n, sizeof(unsigned int));
-  for (unsigned int i = 0; i < k; i++){
-    conjunto[(c[i].linha - 1) * n + c[i].coluna - 1] = 1;
-  }
-  for (unsigned int i = 0; i < n; i++) {
-    unsigned int contcol = 0, contlin = 0;
+unsigned int *rainhas_ci(unsigned int n, unsigned int k, casa *c, unsigned int *r){
+  for (unsigned int i = 0; i < n; i++){
     r[i] = 0;
-    for (unsigned int j = 0; j < n; j++) {
-      if (conjunto[j * n + i]) {
-        contcol++;
-      }
-      if (conjunto[i * n + j]) {
-        contlin++;
-      }
-    }
-    if (contcol == n){
-      contadorcol++;
-    }
-    if (contlin == n){
-      contadorlin++;
+  }
+  unsigned int *tabuleiro = calloc(n * n, sizeof(unsigned int));
+  for (int i = 0; i < k; i++){
+    tabuleiro[(c[i].linha - 1) * n + c[i].coluna - 1] = 1;
+  }
+  for (unsigned int i = 0; i < n; i++){
+    if (rainhas_ci_wrapped(n, k, c, r, tabuleiro, 0, i)){
+      free(tabuleiro);
+      return r;
     }
   }
-  if (contadorcol > contadorlin){
-    for (unsigned int i = 1; i <= contadorcol - contadorlin; i++){
-      for (unsigned int j = 0; j < n; j++){
-        conjunto[i*n + j] = 1;
-      }
-    }
-  }
-  rainhas_ci_wrapped(conjunto, n, r, conjunto);
-  free(conjunto);
+  free(tabuleiro);
   return r;
 }
--- trabalho-1/teste.c
+++ trabalho-1/teste.c
@@ -24,40 +24,16 @@ static void mostra_resposta(unsigned int n, unsigned int *r) {
 
 static casa *proibe_diagonais(unsigned int n, casa *proibido) {
 
-  for (int i = 0; i<7; i++){
-    proibido[i].linha = 1;
-    proibido[i].coluna = i + 1;
-  }
-  for (int i = 7; i<13; i++){
-    proibido[i].linha = 2;
-    proibido[i].coluna = i - 6;
-  }
-  for (int i = 13; i<18; i++){
-    proibido[i].linha = 3;
-    proibido[i].coluna = i - 12;
-  }
-  for (int i = 18; i<23; i++){
-    proibido[i].linha = 5;
-    proibido[i].coluna = i - 17;
-    proibido[i+5].linha = 6;
-    proibido[i+5].coluna = i - 17;
-    proibido[i+10].linha = 7;
-    proibido[i+10].coluna = i - 17;
-    proibido[i+15].linha = 8;
-    proibido[i+15].coluna = i - 17;
-  }
-
 
   // proíbe todas as casas nas diagonais
-  // for(unsigned int i = 0, p = 1; i < 2 * n; i+=2, p++) {
+  for(unsigned int i = 0, p = 1; i < 2 * n; i+=2, p++) {
+    // diagonal "principal"
+    proibido[i].linha = proibido[i].coluna = p;
 
-  //   // diagonal "principal"
-  //   proibido[i].linha = proibido[i].coluna = p;
-
-  //   // "outra" diagonal
-  //   proibido[i+1].linha = p;
-  //   proibido[i+1].coluna = n - p + 1;
-  // }
+    // "outra" diagonal
+    proibido[i+1].linha = p;
+    proibido[i+1].coluna = n - p + 1;
+  }
 
   return proibido + 2*n;
 }
@@ -65,13 +41,13 @@ static casa *proibe_diagonais(unsigned int n, casa *proibido) {
 //------------------------------------------------------------------------------
 int main (void) {
 
-  unsigned int n = 8;
+  unsigned int n = 16;
   unsigned int *resposta = malloc(n*sizeof(unsigned int));
 
-  unsigned int k = 38;
+  unsigned int k = 2*n;
   casa *proibido = malloc(k*sizeof(casa));
   
-  proibe_diagonais(k, proibido);
+  proibe_diagonais(n, proibido);
 
   printf("backtracking: ");
   long int tempo_bt;
